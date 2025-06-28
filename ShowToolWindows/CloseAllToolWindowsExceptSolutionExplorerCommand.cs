88b8dd31a9cadca4fc652d0b3fb36e52fdea6925
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-06-28 16:29:09 +0100

    Add command to close all tool windows except Solution Explorer
    Introduces `CloseAllToolWindowsExceptSolutionExplorerCommand`
    in the `ShowToolWindows` namespace. This command closes all
    tool windows in Visual Studio except for the Solution Explorer.

---
+++ ShowToolWindows/CloseAllToolWindowsExceptSolutionExplorerCommand.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using Task = System.Threading.Tasks.Task;
+
+namespace ShowToolWindows
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class CloseAllToolWindowsExceptSolutionExplorerCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 4129;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("a2a7f2af-5e8b-4a37-a078-5a8dbe625606");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CloseAllToolWindowsExceptSolutionExplorerCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private CloseAllToolWindowsExceptSolutionExplorerCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static CloseAllToolWindowsExceptSolutionExplorerCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in CloseAllButSolutionExplorerCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new CloseAllToolWindowsExceptSolutionExplorerCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            try
+            {
+                // Get the Solution Explorer tool window
+                var dte = Package.GetGlobalService(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
+                if (dte == null)
+                {
+                    Debug.WriteLine("ERROR: Could not get DTE service");
+                    return;
+                }
+
+                // Iterate all windows and close all tool windows except Solution Explorer
+                foreach (EnvDTE.Window window in dte.Windows)
+                {
+                    try
+                    {
+                        if (window.Kind == WindowKindConsts.ToolWindowKind && window.ObjectKind != EnvDTE.Constants.vsWindowKindMainWindow && window.ObjectKind != EnvDTE.Constants.vsWindowKindSolutionExplorer)
+                        {
+                            window.Close();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine($"Could not close window: {window.Caption}, {ex.Message}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the error or show a message
+                Debug.WriteLine($"ERROR closing tool windows: {ex.Message}");
+                Debug.WriteLine($"Stack trace: {ex.StackTrace}");
+            }
+        }
+    }
+}
---
+++ ShowToolWindows/WindowKindConsts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowToolWindows
+{
+    internal class WindowKindConsts
+    {
+        public const string ToolWindowKind = "Tool";
+    }
+}
