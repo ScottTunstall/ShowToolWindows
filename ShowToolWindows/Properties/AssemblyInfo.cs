1f0fc2fa2c48352f730972537b45298fe256fa7d
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-06-28 16:35:56 +0100

    Bump version to 2.0 and update assembly information
    - Changed assembly version and file version to "2.0.*".
    - Updated package version in `source.extension.vsixmanifest` to "2.0".

--- ShowToolWindows/Properties/AssemblyInfo.cs
+++ ShowToolWindows/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ShowToolWindows")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Shows or hides tool windows")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ShowToolWindows")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright 2025 Scott Tunstall.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0.*")]
