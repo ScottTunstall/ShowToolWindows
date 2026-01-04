82c43d7a99427e672d18fc713b29f7d139fa9c4e
Scott Tunstall <scott.tunstall@ntlworld.com>
2026-01-04 18:00:59 +0000

    chore: remove unused ShowToolWindows.UI using
    Removed unnecessary using directive from ToggleToolWindowsCommand.cs.

--- ShowToolWindows/Commands/ToggleToolWindowsCommand.cs
+++ ShowToolWindows/Commands/ToggleToolWindowsCommand.cs
@@ -1,7 +1,6 @@
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Threading;
-using ShowToolWindows.UI;
 using ShowToolWindows.UI.ToolWindows;
 using System;
 using System.ComponentModel.Design;
